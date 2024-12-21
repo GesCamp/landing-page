f540eed4d2769e35f69264008bf173a952cbe524
Omar Olate Salas <omarolatesalas@MacBook-Pro-de-Omar.local>
2024-12-21 18:51:43 -0300

    fix(components): clean components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -25,7 +25,6 @@ import { AuthService } from '../../services/auth.service';
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
-    FloatInputComponent,
     IndexCarouselComponent,
     LoadingComponent,
     GetGalleriesComponent,
